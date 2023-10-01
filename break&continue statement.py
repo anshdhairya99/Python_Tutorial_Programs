acf76d67f39b7e042ced02e0b3d0d76c173038a2
Ansh <anshinbox44@gmail.com>
2023-10-02 00:52:28 +0530

    Loops

---
+++ break&continue statement.py
@@ -0,0 +1,53 @@
+# Break And Continue Statement In python:----------------------
+
+"""i = 0
+while(i<45):
+    i+=1
+    print(i,end=" ")"""
+
+# Break Statement In Python :----
+
+"""i = 0
+while(True):
+    print(i+1,end=" ")
+    if (i==44):
+      break #stop the loop using of break .
+    i+=1"""
+#Continue and break Statement :-----------
+
+"""i=0
+while(True):
+    if i+1<5:
+        i = i+1
+        continue
+    print(i+1,end=" ")
+    if(i==44):
+        break
+    i = i+1"""
+
+# Quiz Question with Solution:---------------------
+
+"""while(True):
+    num = int(input("Enter the number"))
+    if num>=100:
+        print("Congrats🎊🎉 You Have Entered the number")
+        break
+    else:
+        print("Try again")
+        continue"""
+
+"""while(True):
+    num = int(input("Enter the YoU are want that"))
+    if num>=500:
+        print("Congratulation John🎉🎉🎉")
+        break
+    else:
+        print("TRY Again")
+        continue"""
+
+
+
+
+
+
+
