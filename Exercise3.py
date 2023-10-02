dd0f907592ea3b0a97887b465f657450d8325d35
Ansh <anshinbox44@gmail.com>
2023-10-02 14:46:19 +0530

    Quiz No. Of Guesses

--- Exercise3.py
+++ Exercise3.py
@@ -51,11 +51,11 @@ while(number_of_guesses<=9):
         number_of_guesses = number_of_guesses+1
         if(number_of_guesses>9):
             print("Game Over")"""
-n= 18
+"""n= 18
 number_of_guesses = 1
 print("Number of guesses is limited to only 9 times:")
 while(number_of_guesses<=9):
-    guess_number = int(input("guess the number:\n"))
+    guess_number = int(input("Guess the number😊:\n"))
     if guess_number<18:
         print("you enter less number please input greater number\n")
     elif guess_number>18:
@@ -67,4 +67,31 @@ while(number_of_guesses<=9):
     print(9-number_of_guesses,"no. of guesses left" )
     number_of_guesses = number_of_guesses+1
     if(number_of_guesses>9):
-        print("Game over")
+        print("Game over!!❌❌❌")"""
+
+#n = 18
+# no of guesses 9
+# print no of guesses left
+# No of guesses he took to finish
+#game over-
+n= 18
+number_of_guesses=1
+print("Number of guesses is limited to only 9 times:")
+while(number_of_guesses<=9):
+    guess_number=int(input("Guess the Number"))
+    if guess_number>18:
+        print("Print the graeter of 18 numbers")
+    elif guess_number<18:
+        print("Print the smaller of 18 numbers ")
+    else:
+        print("you Won!")
+        break
+    print(9-number_of_guesses,"no. of guesses left")
+    number_of_guesses = number_of_guesses+1
+    if(number_of_guesses>9):
+        print("Game Over!!")
+
+    
+
+
+
