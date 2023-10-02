ca38881d96440c6ea98cbf2e4aca32a488bd9c5a
Ansh <anshinbox44@gmail.com>
2023-10-02 14:14:00 +0530

    while loops

--- While Loops17.py
+++ While Loops17.py
@@ -1,76 +1,76 @@
 # While loop in python:-----------
 
-"""i = 0
+i = 0
 while(i<46):
     print(i)
-    i+=1"""
+    i+=1
 
 # Question In python:----
 
 # Write a program first 10 even number.
-"""num = 2
+num = 2
 while(num<=20):
     num+=2
-    print(num)"""
+    print(num)
 
 #Write a program first 10 odd number.
-"""num = 1
+num = 1
 while(num<=20):
     num+=2
-    print(num)"""
+    print(num)
 
 # Write a program first 10 natural number.
-"""num = 0
+num = 0
 while(num<=12):
     num+=1
-    print(num)"""
+    print(num)
 
 # Write a program first 10 whole number
-"""num =0
+num =0
 while(num<100):
     num+=1
-    print(num)"""
+    print(num)
 
 # Write a program to print first 10 integer and their squares..............................
-"""num = 1
+num = 1
 print("Numbers\t\tSquares")
 while(num<=10):
     num+=1
-    print(num,"\t\t",num**2)"""
+    print(num,"\t\t",num**2)
 
 # write  for loop  statement to print following series:
 # 10,20,30....300
-"""num = 10
+num = 10
 while(num<=290):
     num+=10
-    print(num)""" 
+    print(num)
 
 #write a while loop statement to print the following series 107,98,91.......7
 
-"""num =105
+num =105
 while(num>=7):
     num-=7
-    print(num)"""
+    print(num)
 
-"""num = 11
+num = 11
 while(num>=1):
     num-=1
-    print(num)"""
+    print(num)
 
-"""num =2
+num =2
 while(num<=20):
     num+=2
-    print(num)"""
+    print(num)
 
-"""num =1 
+num =1 
 while(num<=20):
     num+=2
-    print(num)"""
+    print(num)
 
-"""num = 3
+num = 3
 while(num<=20):
     num+=3
-    print(num)"""
+    print(num)
 
 
 
