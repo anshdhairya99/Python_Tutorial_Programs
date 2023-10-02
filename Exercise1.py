421bdafedb99f581d0df4e07253a6932e5c3d0af
Ansh <anshinbox44@gmail.com>
2023-10-02 14:18:00 +0530

    Quiz Find discription of Sets

--- Exercise1.py
+++ Exercise1.py
@@ -4,7 +4,7 @@ from  the dictionary."""
 
 """Solution of the above question"""
 
-"""d3 = {"SET":"https://byjus.com/maths/what-is-a-set/",
+d3 = {"SET":"https://byjus.com/maths/what-is-a-set/",
       "TABLE":"A TABLE IS THE THERE ARE MANY NUMBERS ARE IN THE TABLE",
       "VARIABLE":"A VARIABLE IS THE COLLECTION OF CHARACTER",
       "ALPHABET":"A ALPHABET IS THE COLLECTION OF THE ALPHABETS",
@@ -12,19 +12,19 @@ from  the dictionary."""
 }
 print("Enter the Any Variable name")
 inpnum = input()
-print(d3[inpnum])"""
+print(d3[inpnum])
 
 """Question - To create a dictionary and taken from the user and return the  meaning of the word
 from  the dictionary."""
 
-"""d4= {"List":"List are mutable we can change the list very easily",
+d4= {"List":"List are mutable we can change the list very easily",
      "Tuples":"Tuples are immutable we can not change it.",
      "character":"Character is the letter of the alphabets",
      "Variables":"Variable is the exclude of whole digits in var",}
 print("Enter the any vairables name")
 num = input()
 print(d4[num]) 
-print(type(d4))"""
+print(type(d4))
 
 d5 = {"List":"List are mutable we can change the list very easily",
       "Tuples":"Tuples are immutable we can not change it",
