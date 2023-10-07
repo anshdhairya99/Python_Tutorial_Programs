6a57a167e54d3c75e56bd00b3b4cd4f60d502889
Ansh <anshinbox44@gmail.com>
2023-10-07 11:50:22 +0530

    short hand if else notation

--- shorthand.py
+++ shorthand.py
@@ -1,5 +1,15 @@
-# short hand if else notation in python:-
+#Short hand if else notation in python:--------------------------------
+
+
+# The Code In One Line :-----------------------------------------------------------------------
 a = int(input("Enter a\n"))
 b = int(input("Enter b\n"))
-#if a>b: print("A B se bada hai bhai")
+if a>b: print("A B se bada hai bhai") 
 print("B A se bada hai bahi") if a<b else print("A B se bada hai bhai")
+
+
+m = int(input("Enter the number1\n"))
+n = int(input("Enter the number2\n"))
+if a>b: print("It is greater than b")
+print("B is greater than A") if b>a else print("It is greater.")
+
