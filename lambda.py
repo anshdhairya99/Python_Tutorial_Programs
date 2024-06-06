f591433fa8c6f0a17acfccc1af03873fcdbb019b
Ansh <anshinbox44@gmail.com>
2024-06-06 18:12:21 +0530

    lambda

--- lambda.py
+++ lambda.py
@@ -13,14 +13,15 @@ multiply = lambda x, y: x * y
 print(multiply(3, 5))
  
 # Sort Lambda function:----
+
 def a_first(a):
     return a[1]
+
 
 a = [[2,14],[5,6],[8,23]]
 #a.sort(key=a_first)
 a.sort(key=lambda x:x[1])
 print(a)
-
 
 
 
