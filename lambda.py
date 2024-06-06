6a3f645520952799790202d64d417e12f83b6252
Ansh <anshinbox44@gmail.com>
2024-06-06 18:04:26 +0530

    lambda

--- lambda.py
+++ lambda.py
@@ -1,17 +1,29 @@
 # Lambda Function and Anonymous Function in python:----
 
-# def add(a, b):
-#     return a+b
+def add(a, b):
+    return a+b
 
-# minus = lambda x, y: x-y
+minus = lambda x, y: x-y
 
 def minus(x,y):
     return x-y
 
 print(minus(9,4))
 multiply = lambda x, y: x * y
-
 print(multiply(3, 5))
+ 
+# Sort Lambda function:----
+def a_first(a):
+    return a[1]
+
+a = [[2,14],[5,6],[8,23]]
+#a.sort(key=a_first)
+a.sort(key=lambda x:x[1])
+print(a)
+
+
+
+
 
 
 
