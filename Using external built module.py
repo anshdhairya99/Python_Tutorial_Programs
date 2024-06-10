0d4959a7232f7d29dce8191855b9d60ccfc69248
Ansh <anshinbox44@gmail.com>
2024-06-10 15:36:30 +0530

    Module

---
+++ Using external built module.py
@@ -0,0 +1,19 @@
+# Using External built module in Python:----
+# They are built in module:---
+import random
+
+random_number = random.randint(0,1)
+print(random_number)
+
+rand = random.random()*100
+print(rand)
+
+
+#Random Module:--
+lst = ["AAj Tak","Star Plus","DD1","Natgeo","Discovery"]
+
+choice = random.choice(lst)
+print(choice)
+
+
+
