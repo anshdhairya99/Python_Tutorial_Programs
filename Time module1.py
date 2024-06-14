10ee3289b8482aba2a1ef866a1cfb1702006cc3b
Ansh <anshinbox44@gmail.com>
2024-06-14 12:41:24 +0530

    Time Module

--- Time module1.py
+++ Time module1.py
@@ -4,12 +4,31 @@ import time
 initial = time.time()
 k = 0
 while(k<45):
-    print("This is harry bhai")
+    print("This is the good guy!")
     k+=1
-    print("While loop ran in",time.time()-initial,"Second")
+    print("While loop ran in",time.time()-initial,"seconds")
 
 initial2 = time.time()
 for i in range(45):
-    print("This is harry bhai")
+    print("For Loop ran in",time.time-initial2())
 
-print("For Loop ran in",time.time()-initial2,"seconds")
+
+import time
+initial = time.time()
+k = 0
+while(k<45):
+    print("Hello Mac Donalds can you make the recpie fast")
+    time.sleep(2) #written the code within the gap of 2 seconds
+    k+=1
+    print("While loop ran in",time.time()-initial,"seconds")
+
+initial2 = time.time()
+for i in range(45):
+    print("Hello Subway boys!")
+    print("For Loop  ran in",time.time()-initial2,"seconds")
+
+# To Calculate the time show the time present and year :--
+# import time
+
+localtime =time.asctime(time.localtime(time.time()))
+print(localtime)
