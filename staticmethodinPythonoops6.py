f0610226bb66d1b0702670ffeb708b7702ee04c0
Ansh <anshinbox44@gmail.com>
2024-07-19 13:07:30 +0530

    classmethodas alternate

---
+++ staticmethodinPythonoops6.py
@@ -0,0 +1,29 @@
+# *******************************STATIC METHOD IN PYTHON**********************************************
+
+class Employee:
+    no_of_leaves = 8
+    def __init__(self,aname,asalary,arole):
+        self.name = aname
+        self.salary = asalary
+        self.role = arole
+        
+    def printdetails(self):
+        return f"The Name is{self.name}.Salary is{self.salary}.And Role is{self.role}"
+    @classmethod
+    def change_leaves(cls,newleaves):
+        cls.no_of_leaves = newleaves
+
+    @classmethod
+    def from_dash(cls,string):
+        return cls(*string.split("-"))
+    @staticmethod
+    def printgood(string):
+
+        print("This is good" +string)
+harry = Employee("Harry",255,"Instructor")
+rohan = Employee("Rohan",455,"Students")
+karan = Employee.from_dash("karan-480-stdents")
+print(karan.no_of_leaves)
+karan.printgood("Harry") # karan is the instance
+Employee.printgood("Harry") # Employee is the class
+Employee.printgood("Rohan")
