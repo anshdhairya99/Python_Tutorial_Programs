6a852c9b86400e35f16927691fb6a57c27013439
Ansh <anshinbox44@gmail.com>
2024-08-02 11:52:27 +0530

    Multilevel Inheritence oops9

--- Multilevel Inheritence oops9.py
+++ Multilevel Inheritence oops9.py
@@ -21,6 +21,30 @@ harry = Grandson()
 print(harry.isdance())
 print(harry.basketball)
 
-# Make a 3 class of electronic device, pocket gadget & phone using of multilevel inheritence:-
+# Make a 3 class of Electronic device, Pocket gadget & Phone using of multilevel inheritence:-
 
+class ElectronicsDevice:
+    Ceilingfan = 12
+    
+class PocketGadget(ElectronicsDevice):
 
+    Money =100
+    Ceilingfan = 122
+
+    def isMoney(self):
+        return f"Yes I send the money{self.Money} of the company"
+    
+class Phone(PocketGadget):
+    Money = 10
+
+    # def isMoney(self):
+    #     return f"Hello Yeah!"\
+    #         f"Yes I need too much Money"
+
+karan = ElectronicsDevice()
+maran = PocketGadget()
+saran = Phone()
+
+print(saran.isMoney())
+
+print(saran.Ceilingfan)
